d8c061818bebb38faa9bdfbf28f45e5b1194e751
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-08-15 17:31:52 +0900

    Create baekjoon_11718.java

---
+++ baekjoon_11718.java
@@ -0,0 +1,23 @@
+package baekjoon_java;
+
+
+import java.util.Scanner;
+
+public class baekjoon_11718 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s;
+
+        while(scan.hasNext()) {
+            s=scan.nextLine();
+            System.out.println(s);
+        }
+
+
+
+
+
+
+    }
+
+}
