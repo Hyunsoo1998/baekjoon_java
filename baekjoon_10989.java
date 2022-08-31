244c1bb76cf5d64046de080ad528bc2da1264498
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-08-31 16:20:56 +0900

    Create baekjoon_10989.java
    원래 내가 작성한 코드는 메모리초과로 인해풀지못함.

---
+++ baekjoon_10989.java
@@ -0,0 +1,41 @@
+package baekjoon_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import java.util.StringTokenizer;
+
+public class baekjoon_10989 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        int arr[] = new int[N];
+
+        for(int i = 0 ; i<N; i++) {
+
+            arr[i]=Integer.parseInt(br.readLine());
+
+        }
+
+        Arrays.sort(arr);
+
+
+        for(int i =0 ; i <N; i ++) {
+            sb.append(arr[i]).append('\n');
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
