e3b94e1c2ef93ab0ca13abe9595138985aa79264
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-09-01 10:10:22 +0900

    baekjoon_10718,2231
    2231번은 실패하고
    https://st-lab.tistory.com/98
    사이트를 참조해서 문제를 풀음
    2231은 브루트포스 문제

---
+++ baekjoon_10718.java
@@ -0,0 +1,8 @@
+package baekjoon_java;
+
+public class baekjoon_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
