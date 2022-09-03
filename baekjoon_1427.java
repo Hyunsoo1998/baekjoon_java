28372dd1509b64510565e1b5827b2af36ba9c4bc
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-09-03 17:46:10 +0900

    Create baekjoon_1427.java

---
+++ baekjoon_1427.java
@@ -0,0 +1,19 @@
+package baekjoon_java;
+
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toCharArray();
+
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
