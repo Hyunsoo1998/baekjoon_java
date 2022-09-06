fdfbc82d13527bd1321eb41b7a31238e38b61490
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-09-06 13:53:34 +0900

    Create baekjoon_7568.java
    백준 단계별로 푸는문제 브루트포스 7568

---
+++ baekjoon_7568.java
@@ -0,0 +1,34 @@
+package baekjoon_java;
+
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_7568 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[][] = new int[N][2];
+        String [] sp;
+        for(int i = 0; i < N; i++) {
+            sp = br.readLine().split(" ");			// 문자열 분리
+            arr[i][0] = Integer.parseInt(sp[0]);	// [i][0] : 몸무게
+            arr[i][1] = Integer.parseInt(sp[1]);	// [i][1] : 키
+        }
+
+
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+        }
+}
