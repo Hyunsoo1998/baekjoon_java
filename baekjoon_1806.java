cdc937f59fabf00fa8b8790d8d89c35fffc7ebb9
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-10-05 18:07:21 +0900

    baekjoon_1806_2Pointer
    백준 1806 투 포인터 문제.
    처음에 L값 R값으로 줄여나가면 될 수있지 않을까 생각했지만, 연속으로 주어진 길이의 최소기 때문에 아이디어 유기.
    
    정렬을 사용하려 했지만 정렬을 사용하는 순간 문제 조건 위반..
    
    그래서 처음을 고정해놓고 하나를 계속 늘려가면 되지않을까? 생각했다.
    하지만 알고리즘 구현하는 능력이 모자라서 아이디어만 떠오르고 제대로 구현을 하지 못했다..

---
+++ baekjoon_1806.java
@@ -0,0 +1,62 @@
+package baekjoon_java;
+
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_1806 {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N, S;
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+
+        Input();
+
+
+    }
+
+    static void Input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+        int first = 0;
+        int second = 0;
+        int sum = 0;
+        int min = 100001;
+
+        while (true) {
+            if (sum < S) {
+                sum += arr[second++];
+
+            } else if (N == second) {
+                break;
+
+            } else {
+                sum -= arr[first++];
+
+                min = Math.min(min, ((second - first) + 1));
+
+            }
+
+
+        }
+        if(min==100001) {
+            System.out.println(0);
+
+        }else{
+            System.out.println(min);
+
+
+            }
+        }
+    }
