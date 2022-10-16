a5a3f2f87a7b64af51b54c96f83564095aac1739
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-10-16 13:28:36 +0900

    Update boj_17298_sol.java

--- boj_17298_sol.java
+++ boj_17298_sol.java
@@ -10,7 +10,7 @@ public class boj_17298_sol {
 
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
         int[] seq = new int[N];
@@ -25,12 +25,7 @@ public class boj_17298_sol {
 
         for(int i = 0; i < N; i++) {
 
-            /*
-             * 스택이 비어있지 않으면서
-             * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
-             * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
-             * 해당 인덱스의 값을 현재 원소로 바꿔준다.
-             */
+
             while(!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
                 seq[stack.pop()] = seq[i];
             }
@@ -38,10 +33,7 @@ public class boj_17298_sol {
             stack.push(i);
         }
 
-        /*
-         * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
-         * -1로 초기화한다.
-         */
+
         while(!stack.isEmpty()) {
             seq[stack.pop()] = -1;
         }
